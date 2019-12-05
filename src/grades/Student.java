e647c246a2aec9d8026651b49ccd5b2f313d9fe4
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2019-12-05 11:31:43 -0600

    finishing first

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -56,28 +56,4 @@ public class Student {
             System.out.println("/" + attendance.get(dates));
         }
     }
-
-
-
 }
-
-
-
-
-
-//    Create a class named Student. It should have private properties for the student's
-//        name, and grades. The grades property should be an ArrayList of integers.
-//        The student class should have a constructor that sets name property,
-//        and initializes the grades property as an empty ArrayList.
-//        The Student class should have the following methods:
-//
-//
-//// returns the student's name
-//public String getName();
-//// adds the given grade to the grades property
-//public void addGrade(int grade);
-//// returns the average of the students grades
-//public double getGradeAverage();
-//        Test your Student class by adding a main method and creating Student objects.
-//        Verify that you can add grades to each object, and that your getGradeAverage method
-//        correctly returns the average of the student's grades.
